109720ca1c3039ab1e8269d40caf9799c610b09e
HoodyHuo <rnmdo@163.com>
2019-08-09 09:57:29 +0800

    yml property factory

---
+++ src/main/groovy/vip/hoody/wechat/utils/YamlPropertySourceFactory.java
@@ -0,0 +1,43 @@
+package vip.hoody.wechat.utils;
+
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.support.EncodedResource;
+import org.springframework.core.io.support.PropertySourceFactory;
+import org.springframework.lang.Nullable;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
+import org.springframework.core.env.PropertiesPropertySource;
+
+/**
+ * YAML file loader
+ * @author Marten Deinum
+ * @see "https://mdeinum.github.io/2018-07-04-PropertySource-with-yaml-files/"
+ */
+public class YamlPropertySourceFactory implements PropertySourceFactory {
+
+    public PropertySource<?> createPropertySource(@Nullable String name, EncodedResource resource) throws IOException {
+        Properties propertiesFromYaml = loadYamlIntoProperties(resource);
+        String sourceName = name != null ? name : resource.getResource().getFilename();
+        return new PropertiesPropertySource(sourceName, propertiesFromYaml);
+    }
+
+    private Properties loadYamlIntoProperties(EncodedResource resource) throws FileNotFoundException {
+        try {
+            YamlPropertiesFactoryBean factory = new YamlPropertiesFactoryBean();
+            factory.setResources(resource.getResource());
+            factory.afterPropertiesSet();
+            return factory.getObject();
+        } catch (IllegalStateException e) {
+            // for ignoreResourceNotFound
+            Throwable cause = e.getCause();
+            if (cause instanceof FileNotFoundException) {
+                throw (FileNotFoundException) e.getCause();
+            }
+            throw e;
+        }
+    }
+}
